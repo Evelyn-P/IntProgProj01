0b91413570a06ee524ae441f387f65b3a6252d03
Evelyn-P <Evelyn-P@Hoseki-v2.local>
2016-02-26 05:36:16 +0700

    added devise

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,6 @@
+#ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.smtp_settings = {
+  #:address              => 'smtp.sendgrid.net',
+  #:port                 => '587'
+  #:enable_starttls_auto => 'true'
+#}
